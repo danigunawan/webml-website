8f9b22e82ac36b3f9387c2a5b89a7e51d532b38b
Belem Zhang <belem.zhang@intel.com>
2019-01-30 10:51:17 +0800

    Model URL updates

--- examples/static/js/model.js
+++ examples/static/js/model.js
@@ -36,7 +36,7 @@ function showModel(div, modelcategory) {
     let modelUrl = new URL(model.modelFile.replace('../', '../examples/'), location.href);
 
     if(location.hostname.indexOf('.github.io') >-1) {
-      let user = location.replace('.github.io', '');
+      let user = location.hostname.replace('.github.io', '');
       let path = location.pathname.split('/')[1];
       let newlocation = `https://${user}.github.io/${path}/raw/master`;
       modelUrl = new URL(model.modelFile.replace('../', '../examples/'), newlocation);
